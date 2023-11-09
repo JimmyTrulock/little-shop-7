66602aad6f6911739a1315a953311f0aeb9a0c07
Charles Ren <cren04@syr.edu>
2023-11-08 21:30:25 -0800

    US 37

--- app/services/unsplash_service.rb
+++ app/services/unsplash_service.rb
@@ -7,8 +7,9 @@ class UnsplashService
     url = "https://api.unsplash.com/photos/?client_id=SuPXGqGpPv32qZpl9DNNZX7hO-KzwTihkP2cE19sRhs"
 
     response = Faraday.get(url, {}, { "Authorization" => "Client-ID #{access_key}" })
-    result = JSON.parse(response.body)
-    
+    result = JSON.parse(response.body)  
+    # require 'pry';binding.pry
+
     photo_url = result[0]["urls"]["regular"]
     # require 'pry';binding.pry
   end
