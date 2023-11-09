2c936f4474659baec661df4aac4286bc01fe6b7c
Charles Ren <cren04@syr.edu>
2023-11-08 21:27:50 -0800

    US 37, api photo for every page

--- app/controllers/photos_controller.rb
+++
@@ -1,5 +0,0 @@
-class PhotosController < ApplicationController
-  def index
-    @photos = UnsplashService.search(params[:query]) if params[:query].present?
-  end
-end
--- app/services/unsplash_service.rb
+++ app/services/unsplash_service.rb
@@ -1,10 +1,15 @@
+require 'faraday'
+require 'json'
+
 class UnsplashService
-  def self.search(query)
-    access_key = 'SuPXGqGpPv32qZpl9DNNZX7hO-KzwTihkP2cE19sRhs' # Replace with your actual Unsplash Access Key
-    url = "https://api.unsplash.com/search/photos"
+  def self.fetch_random_logo
+    access_key = 'SuPXGqGpPv32qZpl9DNNZX7hO-KzwTihkP2cE19sRhs'
+    url = "https://api.unsplash.com/photos/?client_id=SuPXGqGpPv32qZpl9DNNZX7hO-KzwTihkP2cE19sRhs"
 
-    response = Faraday.get(url, { query: query }, { "Authorization" => "Client-ID #{access_key}" })
-
-    JSON.parse(response.body)
+    response = Faraday.get(url, {}, { "Authorization" => "Client-ID #{access_key}" })
+    result = JSON.parse(response.body)
+    
+    photo_url = result[0]["urls"]["regular"]
+    # require 'pry';binding.pry
   end
 end
